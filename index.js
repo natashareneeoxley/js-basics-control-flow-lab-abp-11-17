da2493aa63416c4cb159d4a67c207bc49d4bcba7
Natasha Oxley <natashareneeoxley@gmail.com>
2017-11-26 16:33:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(199){
+  if (199 <= 400 feet) {
+    return "This one is on me!";
+  } else if (199 > 400 feet) {
+    return "I will gladly take your thirty bucks."  
+  }
+}
+
+
+
+
