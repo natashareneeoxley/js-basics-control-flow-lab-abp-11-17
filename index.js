d1aa571c4241309264a60522a4e0b2fb0e7236bd
Natasha Oxley <natashareneeoxley@gmail.com>
2017-11-26 17:26:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 function scuberGreetingForFeet(199){
   if (scuberGreetingForFeet(199) <= 400) {
-    console.log('This one is on me!');
+    console.log('This one is on me!')
   } else if (scuberGreetingForFeet(2001) > 2002) {
-    console.log('I will gladly take your thirty bucks.');
+    console.log('I will gladly take your thirty bucks.')
   } else if (scuberGreetingForFeet(2501) <= 2001) {
-    console.log('I will gladly take your thirty bucks.');
+    console.log('I will gladly take your thirty bucks.')
   }
 }
